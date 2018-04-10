3ef0d1c6891c79b182d4d6cc76c98b3b96298e74
Taylor Emolo <taylor.emolo@gmail.com>
2018-04-10 08:23:16 -0400

    manifest.json edited

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize.js';
 import Nav from './components/Nav';
-// import Header from './components/Header';
-// import OurWork from './components/OurWork';
-// import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
-// import Home from './components/Home';
-// import About from './components/About';
-// import WhyFutureFownders from './components/WhyFutureFownders';
 
 class App extends Component {
   render() {
