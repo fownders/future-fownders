e233aca6670906f399ebd9f2172cadb1e9acd949
EstherMx <esmaxi21@gmail.com>
2018-05-01 14:50:39 -0400

    partners and team

--- src/components/OurWork/Team.js
+++ src/components/OurWork/Team.js
@@ -9,22 +9,24 @@ const Team = () => {
                         <h2 className="black-text mt0">Meet Our Team</h2>
                     </div>
                     <div className="col s12 m4">
-                        <img className="responsive-img" alt="Amanda Johnson Fownders Director" src="https://fownders.s3.us-east-2.amazonaws.com/amanda.png" />
+                        <img className="responsive-img" alt="Amanda Johnson Fownders Director" src="https://farm1.staticflickr.com/831/41696465251_df6ffc1aa0_m.jpg" />
                     </div>
                     <div className="col s12 m8 left-align">
                         <p className="brand-text bold mb0">Amanda Johnson</p>
                         <p className="black-text mt0">Director</p>
-                        <p className="black-text">Amanda is the Director of Future Fownders. Since children are our future, we believe that they need to be ready for anything that life throws at them. Throw the Future Fownders program, we help young in our community develop the wisdom to overcome future obstacles that come their way.</p>
+                        <p className="black-text">Amanda Johnson: Born and raised in Newark, NJ, Amanda believes that our young people need to be ready for anything that life throws at them. Through the Future Fownders program, she works passionately to help young people in our community develop the confidence and fortitude to overcome future obstacles that come their way. 
+</p>
                     </div>
                 </div>
                 <div className="row mb0">
                     <div className="col s12 m4">
-                        <img className="responsive-img" alt="Jade Jordan Fownders Operations Assistant" src="https://fownders.s3.us-east-2.amazonaws.com/jade.png" />
+                        <img className="responsive-img" alt="Jade Jordan Fownders Operations Assistant" src="https://farm1.staticflickr.com/909/40797676665_7426aa8c66_m.jpg" />
                     </div>
                     <div className="col s12 m8 left-align">
                         <p className="brand-text bold mb0">Jade Jordan</p>
                         <p className="black-text m0">Operations Assistant</p>
-                        <p className="black-text">Jade Jordan is the Director of Community Engagement here at Fownders. As the head of community outreach, Jade serves as the point-person for partnering with local community-based organizations in order to help Fownders actively engage with various community partnerships, raise awareness in the city and create a lasting positive impact in the community.</p>
+                        <p className="black-text">Jade Jordan: Born in Newark, NJ, Jade has taught and worked with local inner-city youth since 2010. She believes that each of our youth has an innate light and power that needs to be cultivated and shared for both personal fulfillment and social impact. She hopes to impart and inspire the same within the youth of Future Fownders. Jade’s interests include art and philosophy. 
+</p>
                     </div>
                 </div>
             </div>
